4eafab52ad67f7b0406751463809e1876aa2afa7
Satish Narayan <narayansatish97@gmail.com>
2022-01-15 16:58:12 +0530

    vowel counter

---
+++ VowelCounter/index.js
@@ -0,0 +1,18 @@
+function countVowel() {
+
+    let str = document.getElementById("input").value;
+    if (str == "")
+        return alert("Empty Input")
+
+    let arr = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    for (i = 0; i < str.length; i++)
+        if (arr.includes(str[i]))
+            count++;
+
+    document.getElementById("result").innerHTML = count + " Vowels.";
+
+
+}
+
+
